83b1fbe8d3b2e4fbdf42286c00a18c67c25a198f
Taffroi <taffroi@proton.me>
2025-07-14 02:50:10 +0200

    scroll transition JAVASCRIPT

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 let lastScrollY = 0;
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY > lastScrollY) {
-        nav.style.top = "-80px";
-    } else {
-        nav.style.top = 0;
-    }
+window.addEventListener("scroll", () => {
+  if (window.scrollY > lastScrollY) {
+    nav.style.top = "-80px";
+  } else {
+    nav.style.top = 0;
+  }
 
-    lastScrollY = window.scrollY;
-})
+  lastScrollY = window.scrollY;
+});
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("scroll_show");
+    } else {
+      entry.target.classList.remove(
+        "scroll_show"
+      ); /* Remet l'animation si le visiteur revient en haut puis en bas */
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".scroll_hidden");
+hiddenElements.forEach((el) => observer.observe(el));
