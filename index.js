82f9c9887a2c6d202a0df6a6ddcdee1f47128b16
Taffroi <taffroi@proton.me>
2025-07-26 18:58:41 +0200

    Scroll animation delay & threshold, text animations + fixes
    - Scroll_hidden now applies only to children.
    - Scroll_self added for old behavior
    - Intersection observer now has a threshold of 0.25
    - Added text_fill to the observer
    - Improved (and fixed) primary button animation
    - Fixed SVG Star title on projects (mobile)
    - Adjusted spacing of project_info on mobile
    - Fixed buttons too big on mobile
    - Fixed waves getting on top of other elements

--- index.js
+++ index.js
@@ -10,18 +10,55 @@ window.addEventListener("scroll", () => {
   lastScrollY = window.scrollY;
 });
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("scroll_show");
-    } else {
-      //   entry.target.classList.remove(
-      //     "scroll_show"
-      //   ); /* Remet l'animation si le visiteur revient en haut puis en bas */
-    }
-  });
-});
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting) {
+        entry.target.classList.add("scroll_show");
+      } else {
+        //   entry.target.classList.remove(
+        //     "scroll_show"
+        //   ); /* Remet l'animation si le visiteur revient en haut puis en bas */
+      }
+    });
+  },
+  {
+    threshold: 0.25,
+  }
+);
 
-const hiddenElements = document.querySelectorAll(".scroll_hidden");
+let hiddenElements = document.querySelectorAll(".scroll_hidden"),
+  animateTextElements = document.querySelectorAll(".text_fill");
+
 hiddenElements.forEach((el) => observer.observe(el));
+animateTextElements.forEach((el) => observer.observe(el));
+
+// Code pour que l'étoile des boutons suive le curseur
+
+// let followCursorElement = document.getElementById("caca");
+
+// //Détecte si c'est une tablette/mobile
+// function isTouchDevice() {
+//   try {
+//     document.createEvent("TouchEvent");
+//     return true;
+//   } catch (e) {
+//     return false;
+//   }
+// }
+
+// const move = (e) => {
+//   try {
+//     // PageX et PageY retournent la position du curseur
+//     var x = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
+//     var y = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
+//   } catch (e) {}
+//   followCursorElement.style.left = x + "px";
+//   followCursorElement.style.top = y + "px";
+// };
+
+// // Pour la souris
+// document.addEventListener("mousemove", (e) => {
+//   move(e);
+// });
